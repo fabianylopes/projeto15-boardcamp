06adc957b989ee218f13cdb4c3139ad293b2b834
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-05-26 19:33:46 -0300

    feat: list categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,10 +1,8 @@
 import connection from "../db.js";
 
 export async function getCategories(req, res){
-    
-    const result = await connection.query('SELECT * FROM categorias');
-    
-    console.log(result);
 
-    res.send('ok');
+    const result = await connection.query('SELECT * FROM categories');
+
+    res.send(result.rows);
 }
