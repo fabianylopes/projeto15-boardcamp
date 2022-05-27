b0af5044860cef542023fad9a20aa9f4bee36d27
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-05-26 21:41:19 -0300

    small fix in category route

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -2,16 +2,22 @@ import connection from "../db.js";
 
 export async function getCategories(req, res){
 
-    const result = await connection.query('SELECT * FROM categories');
+    try {
+        const result = await connection.query('SELECT * FROM categories');
+    
+        res.send(result.rows);
+        
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
-    res.send(result.rows);
 }
 
-export async function setCategories(req, res){
-    const category = req.body;
+export async function setCategory(req, res){
+    const { name } = req.body;
 
     try {
-        await connection.query(`INSERT INTO categories (name) VALUES ($1)`, [category.name]);
+        await connection.query(`INSERT INTO categories (name) VALUES ($1)`, [name]);
 
         res.sendStatus(201);
         
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
-import { getCategories, setCategories } from "../controllers/categoriesController.js";
+import { getCategories, setCategory } from "../controllers/categoriesController.js";
 import { validateCategory, validateName } from "../middlewares/validateCategory.js";
 
 const categoriesRouter = Router();
 
 categoriesRouter.get('/categories', getCategories);
-categoriesRouter.post('/categories', validateCategory, validateName, setCategories);
+categoriesRouter.post('/categories', validateCategory, validateName, setCategory);
 
 export default categoriesRouter;
