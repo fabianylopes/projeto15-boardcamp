16d80e4fc9b852aad43ec333963aebb05e03f244
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-05-26 21:06:41 -0300

    feat: insert category

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -6,3 +6,16 @@ export async function getCategories(req, res){
 
     res.send(result.rows);
 }
+
+export async function setCategories(req, res){
+    const category = req.body;
+
+    try {
+        await connection.query(`INSERT INTO categories (name) VALUES ($1)`, [category.name]);
+
+        res.sendStatus(201);
+        
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { getCategories } from "../controllers/categoriesController.js";
+import { getCategories, setCategories } from "../controllers/categoriesController.js";
+import { validateCategory, validateName } from "../middlewares/validateCategory.js";
 
 const categoriesRouter = Router();
 
 categoriesRouter.get('/categories', getCategories);
-//categoriesRouter.post('/categories');
+categoriesRouter.post('/categories', validateCategory, validateName, setCategories);
 
 export default categoriesRouter;
